3ebc6f624e6ef662ed3d12ef55a9938d8a967859
fizzaishfaq110 <fizzaishfaq313@gmail.com>
2025-08-22 12:09:56 +0300

    add: product.ts and api.ts

---
+++ src/services/api.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+import type { Product } from "../types/product";
+
+
+export const getProducts = async (): Promise<Product[]> => {
+    const response = await axios.get("https://fakestoreapi.com/products");
+    return response.data;
+} //not getting data immedietely so async
